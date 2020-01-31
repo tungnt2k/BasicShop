96f0356c382b86939893b614100761007954a108
tungnt2k <tuta1w1aa0000@gmail.com>
2020-01-31 20:01:52 +0700

    initial project

---
+++ README.md
@@ -0,0 +1,22 @@
+# Air VN
+
+This is our first pet project <3
+
+## Getting Started
+
+Basic Shop 
+
+## Built With
+
+* [ExpressJs](https://expressjs.com/) - Fast, unopinionated, minimalist web framework for Node.js
+
+## Authors
+
+* **NGUYỄN THANH TÙNG** - *SV ĐH THỦY LỢI* - [tungnt2k](https://github.com/tungnt2k)
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const homeRoutes = require('./routes/home.routes');
+const adminRoutes = require('./routes/admin.routes');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', homeRoutes);
+app.use('/admin', adminRoutes);
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Something error !!');
+        console.error(err);
+    };
+    console.log('Example app listen on port :' + PORT)
+})
