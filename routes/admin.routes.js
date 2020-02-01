748c1450ba53eeafa5835fbc547a9eace3a2acf3
tungnt2k <tuta1w1aa0000@gmail.com>
2020-02-01 10:13:27 +0700

    add auth admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,61 @@
 const express = require('express');
 const router = express.Router();
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 const controller = require('../controllers/admin');
+const authMiddleware = require('../middlewares/authUser');
+const User = require('../models/User');
 
-router.get('/', controller.index);
+router.get('/', authMiddleware, controller.index);
+router.get('/login', controller.getLogin);
+router.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/admin',
+        failureRedirect: '/admin/login',
+        failureFlash: true
+    })
+);
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+},
+
+    async (username, password, done) => {
+        try {
+            let userStored = await User.findOne({ email: username });
+            if (!userStored) {
+                return done(null, false, { message: "Invalid username !" })
+            }
+            const isMatch = await bcrypt.compare(password, userStored.password);
+            if (isMatch) {
+                return done(null, userStored);
+            } else {
+                return done(null, false, { message: "Invalid password !" })
+            }
+        } catch (err) {
+            console.err(err.message);
+        }
+    }
+
+));
+
+passport.serializeUser((userStored, done) => {
+
+    done(null, userStored.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        let userStored = await User.findById(id);
+        if (userStored) {
+            done(null, userStored);
+        }
+    } catch (err) {
+        console.log(err.message)
+    }
+});
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const config = require('config');
+const bcryptjs = require('bcryptjs');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 const connectDB = require('./config/db');
 connectDB();
+
+//Create admin user 
+// const User = require('./models/User');
+// const salt = bcryptjs.genSaltSync(10);
+// const adminUser = new User({
+//     name: "admin",
+//     email: "admin@test.com",
+//     password: "123456",
+//     type: "ad"
+// });
+// adminUser.password = bcryptjs.hashSync(adminUser.password, salt);
+// adminUser.save();
+
+
 
 const homeRoutes = require('./routes/home.routes');
 const adminRoutes = require('./routes/admin.routes');
@@ -17,9 +35,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 app.use(logger('dev'));
+app.use(session({
+    secret: config.get('Secret'),
+    resave: true,
+    key: 'user',
+    saveUninitialized: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', homeRoutes);
 app.use('/admin', adminRoutes);
